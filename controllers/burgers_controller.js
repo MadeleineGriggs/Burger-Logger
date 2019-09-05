44d3983e06adb6d5ee1fd5abedc6a1066ae83b27
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-09-05 12:56:04 -0400

    Finished ORM, confirmed it is working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,18 +4,34 @@ var burgers = require("../models/burger.js");
 
 
 var router = express.Router();
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
 
-
-router.get("/", function (req, res) {
+router.get("/burgers", function (req, res) {
     //Pass the burger data into the function
-    burger.selectAll(function (data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+    burgers.all(function(burgerData) {
+      // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
+      res.render("index", { burger_data: burgerData });
     });
   });
+
+
+router.post("/burgers/create", function(req, res) {
+
+  burgers.create(req.body.burger_name, function(result) {
+
+    res.redirect("/");
+  });
+});
+
+router.put("/burgers/:id", function(req, res) {
+  burgers.update(req.params.id, function(result) {
+    console.log(result);
+    res.sendStatus(200);
+  });
+});
+
 
 
 
